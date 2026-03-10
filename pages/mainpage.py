245f5594e077c8e7b3fa72d90d9eb29e34c0696b
lafevi21-glitch <lafevi21@gmail.com>
2026-03-10 13:49:36 +0100

    Add files via upload

--- pages/mainpage.py
+++ pages/mainpage.py
@@ -25,6 +25,9 @@ guest = app.session_state.get("logged_user", "Guest")
 
 def mainpage_main():
     app.header("DellyWrite")
+
+    app.divider()
+
     app.subheader("Welcome " + guest)
 
     app.divider()
