3eb218c79f9aadcb164cc0c27d644d5d8a728e6d
lafevi21-glitch <lafevi21@gmail.com>
2026-03-10 13:31:48 +0100

    Add files via upload

---
+++ pages/mainpage.py
@@ -0,0 +1,15 @@
+import streamlit as app
+
+guest = app.session_state.get("logged_user", "Guest")
+
+def mainpage_main():
+    app.header("DellyWrite")
+    app.subheader("Welcome " + guest)
+
+    app.divider()
+
+    # ---------------------------------------------------
+
+    app.subheader("Documents 📄")
+
+    app.button("Create a document", icon="+")
