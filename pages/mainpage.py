7f503c94b2e647d48c1d6e24e93b79895704c3b3
lafevi21-glitch <lafevi21@gmail.com>
2026-03-10 13:42:13 +0100

    Add files via upload

--- pages/mainpage.py
+++ pages/mainpage.py
@@ -1,5 +1,26 @@
 import streamlit as app
 
+if not "logged_user" in app.session_state:
+    app.error("Please, make sure you are logged!")
+    app.switch_page("streamlit_app.py")
+    app.stop()
+
+app.markdown(
+    """
+    <style>
+        /* Hides the sidebar entirely */
+        [data-testid="stSidebar"] {
+            display: none;
+        }
+        /* Hides the small arrow button that opens the sidebar */
+        [data-testid="stSidebarCollapsedControl"] {
+            display: none;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 guest = app.session_state.get("logged_user", "Guest")
 
 def mainpage_main():
