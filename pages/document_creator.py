9b9a1dcb311c4fdf5af88da93619afaa839d2cac
lafevi21-glitch <lafevi21@gmail.com>
2026-03-12 09:59:55 +0100

    Add files via upload

--- pages/document_creator.py
+++ pages/document_creator.py
@@ -1,7 +1,3 @@
 import streamlit as app
 
-app.textarea(
-    "Title",
-    placeholder="My Beautiful Document"
-    height=400
-)
+app.textarea("Title", placeholder="My Beautiful Document", height=400)
