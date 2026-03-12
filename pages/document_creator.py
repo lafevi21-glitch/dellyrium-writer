4d547de87d832699c7453e861a73f2ce97db80b8
lafevi21-glitch <lafevi21@gmail.com>
2026-03-12 10:11:41 +0100

    Add files via upload

--- pages/document_creator.py
+++ pages/document_creator.py
@@ -1,3 +1,12 @@
 import streamlit as app
+from streamlit_quill import st_quill
 
-app.text_area("Title", placeholder="My Beautiful Document", height=400)
+document_body = st_quill(
+    placeholder="Blank body"
+    html=True
+    key="quill_editor"
+)
+
+if document_body:
+    st.markdown("### Preview")
+    st.write(document_body, unsafe_allow_html=True)
